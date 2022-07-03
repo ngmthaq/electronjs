d21b1cfe9061622f8d9ce867af4cb6fff63368be
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-07-03 17:05:19 +0700

    format code

--- src/theme/material/palette.js
+++ src/theme/material/palette.js
@@ -1,48 +1,48 @@
-const black = "#2e2e2e";
-const white = "#f0f0f0";
+const black = "#2e2e2e"
+const white = "#f0f0f0"
 
 const palette = {
   mode: "light",
   common: {
     black: black,
-    white: white
+    white: white,
   },
   primary: {
     main: "#90caf9",
     light: "#e3f2fd",
     dark: "#42a5f5",
-    contrastText: "#fff",
+    contrastText: white,
   },
   secondary: {
     main: "#ce93d8",
     light: "#f3e5f5",
     dark: "#ab47bc",
-    contrastText: "#fff",
+    contrastText: white,
   },
   error: {
     main: "#f44336",
     light: "#e57373",
     dark: "#d32f2f",
-    contrastText: "#fff",
+    contrastText: white,
   },
   warning: {
     main: "#ffa726",
     light: "#ffb74d",
     dark: "#f57c00",
-    contrastText: "#fff",
+    contrastText: white,
   },
   info: {
     main: "#29b6f6",
     light: "#4fc3f7",
     dark: "#0288d1",
-    contrastText: "#fff",
+    contrastText: white,
   },
   success: {
     main: "#66bb6a",
     light: "#81c784",
     dark: "#388e3c",
-    contrastText: "#fff",
-  }
-};
+    contrastText: white,
+  },
+}
 
-export default palette;
+export default palette
